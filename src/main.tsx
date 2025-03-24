4140eb8037704acbc7f0ca84c3a4a67550bfa33d
Sean Hopkins <ratchetclnk55@gmail.com>
2025-03-24 02:15:19 -0400

    removed auth0 req

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { ToastContainer } from 'react-toastify';
 import { BrowserRouter } from 'react-router-dom'
-import { Auth0ProviderWithNavigate } from './common/auth0'
 import { AnimatePresence } from 'framer-motion';
 import 'react-toastify/dist/ReactToastify.css';
 import './global.scss';
@@ -14,11 +13,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <ToastContainer />
     <BrowserRouter>
       <AnimatePresence>
-        <Auth0ProviderWithNavigate>
-          <Providers>
-            <App />
-          </Providers>
-        </Auth0ProviderWithNavigate>
+        <Providers>
+          <App />
+        </Providers>
       </AnimatePresence>
     </BrowserRouter>
   </StrictMode >
