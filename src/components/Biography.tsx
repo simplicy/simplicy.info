c808bdfd44ba4d09039d7a65b765248c298b3ca9
Sean Hopkins <ratchetclnk55@gmail.com>
2025-05-19 15:32:01 -0400

    updated bio

--- src/components/Biography.tsx
+++ src/components/Biography.tsx
@@ -63,13 +63,9 @@ export default function Biography() {
         </div>
         <Divider type="DOUBLE" />
         <div className={styles.content}>
-          I am a 28 year old Software Developer born and raised in South Florida. I have been building desktop applications since I was 16.
-          Since then I have explored Web & Mobile development, Computer Repair, Devops, Game development,
-          AI, Microcontrollers, Micro-Soldering, and Drones.
-          Education is a continuous journey for me, and I am always looking to learn new things, whether it be a new programming language, a new paradigm, or a new skill.
-          Currently, I am working on a few personal projects, including an multi-platform outfit planner, and some rust based desktop applications.
-          <br />
-          Fashion is also big part of my life, wanting to learn more I turned to sewing in order to create my own clothes and bags.
+          I am a 28 year old Software Developer born and raised in South Florida.
+          My free time is split between programming, gaming, sewing, or modding electronics.
+          Currently working on a multi-platform outfit planner and a VoIP/Messaging application.
         </div>
       </CardDouble>
     </div>
