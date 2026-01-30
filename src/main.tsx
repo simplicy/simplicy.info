7ec8cccd7c3c2118c35549ec143608a11244fec2
Simplicy <ratchetclnk55@gmail.com>
2026-01-30 10:49:23 -0500

    fix toast

--- src/main.tsx
+++ src/main.tsx
@@ -6,16 +6,28 @@ import { BrowserRouter } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion';
 import 'react-toastify/dist/ReactToastify.css';
 import './global.scss';
+import './global.css';
 import Providers from './components/page/Providers.tsx';
+import { Flip } from 'react-toastify';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ToastContainer />
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={true}
+      closeOnClick={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      transition={Flip}
+    />
     <BrowserRouter>
       <AnimatePresence>
-          <Providers>
-            <App />
-          </Providers>
+        <Providers>
+          <App />
+        </Providers>
       </AnimatePresence>
     </BrowserRouter>
   </StrictMode >
